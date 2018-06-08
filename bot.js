eded1e99d0762569bdf3a40e6988786ca62aa2a7
MrNeedsMoeny <39712771+MrNeedsMoeny@users.noreply.github.com>
2018-06-08 21:56:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+onst Discord = require("discord.js");
 const client = new Discord.Client();
 
 
@@ -12,7 +12,7 @@ const client = new Discord.Client();
             message.channel.send({
                 embed: {
                     color: 3447003,
-                    description: "MrNeedsMoney, M& M, Tate, Ting, Spear"
+                    description: "MrNeedsMoney, M&M, Tate, Ting, Spear"
                 }
             });
 
