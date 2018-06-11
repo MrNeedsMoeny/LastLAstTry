0d758c60edca8409870813d9e601cf1a84ad18da
MrNeedsMoeny <39712771+MrNeedsMoeny@users.noreply.github.com>
2018-06-11 22:19:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -254,7 +254,7 @@ console.log("Playing BlackJack");
              
              if (message.content.startsWith(prefix + "ting memes")) {
 
-    var ting = Math.floor(Math.random() * 1) + 1;
+    var ting = Math.floor(Math.random() * 4) + 1;
 
     if (ting === 1) {
 
@@ -263,11 +263,21 @@ console.log("Playing BlackJack");
 
     }
 
-    if (card === 2) {
+    if (ting === 2) {
 
-        message.reply("Tails");
+        message.reply("https://www.pinterest.ca/pin/427067977142082269/");
 
-  
+  }
+
+    if (ting === 3) {
+
+        message.reply("https://images.google.ca/imgres?imgurl=https%3A%2F%2Fpics.me.me%2Fwhen-you-holla-at-a-peng-ting-and-her-friends-10591348.png&imgrefurl=https%3A%2F%2Fme.me%2Fi%2Fwhen-you-holla-at-a-peng-ting-and-her-friends-6778355&docid=StuNJCk7V5e4UM&tbnid=YsJ-_CxjjDm9ZM%3A&vet=1&w=500&h=573&source=sh%2Fx%2Fim")
+        
+        }
+
+    if (ting === 4) {
+
+        message.reply("https://goo.gl/images/ZrNB66")
 
      }
 
