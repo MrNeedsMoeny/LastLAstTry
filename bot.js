6e4235ecfe8f66aee7c7145a89d07f7ccb51b2f8
MrNeedsMoeny <39712771+MrNeedsMoeny@users.noreply.github.com>
2018-06-15 23:15:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,8 @@ client.on('message', message => {
               message.channel.send({embed: {
   color: 3447003,
   description: "Heres a list of commands: .card, .flipacoin, .staff, .ting memes and .whoisting"
-}});
+             }
+                                   }});
 
 client.on('message', message => {
 
